d0121cacf295912630c8211bf3fec096f64c2f88
Idealis Consulting <2088903+idealisconsultingbe@users.noreply.github.com>
2020-04-28 08:56:47 +0200

    Add files via upload

---
+++ payment_mollie_official/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import payment_acquirer
+from . import payment_acquirer_method
+from . import payment_icon
+from . import payment_transaction
+from . import provider_log
+from . import res_partner
+from . import account_move
